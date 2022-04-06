0b1e8b9fe01e00b6a12dd5b3afb7504985db93e0
Piotr Miłoszewicz <pmiloszewicz@gmail.com>
2022-04-06 13:05:27 +0200

    fix: `createRoot` import path

--- node_package/src/reactRender.ts
+++ node_package/src/reactRender.ts
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { ReactElement, Component } from 'react';
 import supportsReactCreateRoot from './supportsReactCreateRoot';
 
 export default function reactRender(domNode: Element, reactElement: ReactElement): void | Element | Component {
   if (supportsReactCreateRoot) {
     // @ts-expect-error potentially present if React 18 or greater
-    const root = ReactDOM.createRoot(domNode);
+    const root = createRoot(domNode);
     root.render(reactElement);
     return root
   }
